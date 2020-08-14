d152310a2c64a19bde87700818ed52fa91399b3c
Adam Ralph <adam@adamralph.com>
2020-08-14 14:59:21 +0100

    wip

--- Bullseye/Internal/Logger.View.cs
+++ Bullseye/Internal/Logger.View.cs
@@ -5,6 +5,7 @@ namespace Bullseye.Internal
     using System.Collections.Concurrent;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Text;
     using System.Threading;
     using static System.Math;
 
@@ -14,22 +15,113 @@ namespace Bullseye.Internal
         {
             private readonly ConcurrentDictionary<string, TargetView> targets = new ConcurrentDictionary<string, TargetView>();
 
-            private int targetOrdinal;
+            private int targetCount;
 
-            public State State { get; private set; }
+            public TargetState State { get; private set; }
 
             public TimeSpan? Duration { get; private set; }
 
             public IEnumerable<TargetView> Targets =>
                 this.targets.Values.OrderBy(target => target.Ordinal);
 
-            public TargetView Record(string target, State state, TimeSpan? duration) =>
+            public string ToString(string prefix, Palette p)
+            {
+                // whitespace (e.g. can change to '·' for debugging)
+                var ws = ' ';
+
+                var rows = new List<SummaryRow> { new SummaryRow { TargetOrInput = $"{p.Default}Target{p.Reset}", State = $"{p.Default}Outcome{p.Reset}", Duration = $"{p.Default}Duration{p.Reset}", Percentage = "" } };
+
+                foreach (var targetView in this.Targets)
+                {
+                    var target = $"{p.Target}{targetView.Name}{p.Reset}";
+
+                    var state = targetView.State == TargetState.Failed
+                        ? $"{p.Failed}Failed!{p.Reset}"
+                        : targetView.State == TargetState.NoInputs
+                            ? $"{p.Warning}No inputs!{p.Reset}"
+                            : $"{p.Succeeded}Succeeded{p.Reset}";
+
+                    var duration = targetView.Duration.HasValue
+                        ? $"{p.Timing}{targetView.Duration.Humanize(true)}{p.Reset}"
+                        : "";
+
+                    var percentage = targetView.Duration.HasValue && this.Duration.HasValue && this.Duration.Value > TimeSpan.Zero
+                        ? $"{p.Timing}{100 * targetView.Duration.Value.TotalMilliseconds / this.Duration.Value.TotalMilliseconds:N1}%{p.Reset}"
+                        : "";
+
+                    rows.Add(new SummaryRow { TargetOrInput = target, State = state, Duration = duration, Percentage = percentage });
+
+                    var index = 0;
+
+                    var inputs = targetView.Inputs.ToList();
+                    foreach (var inputView in inputs)
+                    {
+                        var input = $"{ws}{ws}{p.Input}{inputView.Input}{p.Reset}";
+
+                        var inputState = inputView.State == InputState.Failed ? $"{p.Failed}Failed!{p.Reset}" : $"{p.Succeeded}Succeeded{p.Reset}";
+
+                        var inputDuration = inputView.Duration.HasValue
+                            ? $"{(index < inputs.Count - 1 ? p.TreeFork : p.TreeCorner)}{p.Timing}{inputView.Duration.Humanize(true)}{p.Reset}"
+                            : "";
+
+                        var inputPercentage = inputView.Duration.HasValue && this.Duration.HasValue && this.Duration.Value > TimeSpan.Zero
+                            ? $"{(index < inputs.Count - 1 ? p.TreeFork : p.TreeCorner)}{p.Timing}{100 * inputView.Duration.Value.TotalMilliseconds / this.Duration.Value.TotalMilliseconds:N1}%{p.Reset}"
+                            : "";
+
+                        rows.Add(new SummaryRow { TargetOrInput = input, State = inputState, Duration = inputDuration, Percentage = inputPercentage });
+
+                        ++index;
+                    }
+                }
+
+                // target or input column width
+                var tarW = rows.Max(row => Palette.StripColours(row.TargetOrInput).Length);
+
+                // outcome column width
+                var outW = rows.Max(row => Palette.StripColours(row.State).Length);
+
+                // duration column width
+                var durW = rows.Count > 1 ? rows.Skip(1).Max(row => Palette.StripColours(row.Duration).Length) : 0;
+
+                // percentage column width
+                var perW = rows.Max(row => Palette.StripColours(row.Percentage).Length);
+
+                // timing column width (duration and percentage)
+                var timW = Max(Palette.StripColours(rows[0].Duration).Length, durW + 2 + perW);
+
+                // expand percentage column width to ensure time and percentage are as wide as duration
+                perW = Max(timW - durW - 2, perW);
+
+                var builder = new StringBuilder();
+
+                // summary start separator
+                builder.AppendLine($"{prefix}{p.Default}{"".Prp(tarW + 2 + outW + 2 + timW, p.Dash)}{p.Reset}");
+
+                // header
+                builder.AppendLine($"{prefix}{rows[0].TargetOrInput.Prp(tarW, ws)}{ws}{ws}{rows[0].State.Prp(outW, ws)}{ws}{ws}{rows[0].Duration.Prp(timW, ws)}");
+
+                // header separator
+                builder.AppendLine($"{prefix}{p.Default}{"".Prp(tarW, p.Dash)}{p.Reset}{ws}{ws}{p.Default}{"".Prp(outW, p.Dash)}{p.Reset}{ws}{ws}{p.Default}{"".Prp(timW, p.Dash)}{p.Reset}");
+
+                // targets
+                foreach (var row in rows.Skip(1))
+                {
+                    builder.AppendLine($"{prefix}{row.TargetOrInput.Prp(tarW, ws)}{p.Reset}{ws}{ws}{row.State.Prp(outW, ws)}{p.Reset}{ws}{ws}{row.Duration.Prp(durW, ws)}{p.Reset}{ws}{ws}{row.Percentage.Prp(perW, ws)}{p.Reset}");
+                }
+
+                // summary end separator
+                builder.AppendLine($"{prefix}{p.Default}{"".Prp(tarW + 2 + outW + 2 + timW, p.Dash)}{p.Reset}");
+
+                return builder.ToString();
+            }
+
+            public TargetView Record(string target, TargetState state, TimeSpan? duration) =>
                 this.Record(state, duration).Record(target).Record(state, duration);
 
-            public (TargetView, TargetView.InputView) Record(string target, State state, TimeSpan? duration, object input, Guid inputId) =>
-                this.Record(state, duration).Record(target).Record(state, duration, input, inputId);
+            public (TargetView, InputView) Record(string target, InputState state, TimeSpan? duration, object input, Guid inputId) =>
+                this.Record((TargetState)state, duration).Record(target).Record(state, duration, input, inputId);
 
-            private View Record(State state, TimeSpan? duration)
+            private View Record(TargetState state, TimeSpan? duration)
             {
                 this.State = Coalesce(this.State, state);
                 this.Duration = this.Duration.Add(duration);
@@ -37,77 +129,98 @@ namespace Bullseye.Internal
             }
 
             private TargetView Record(string target) =>
-                this.targets.GetOrAdd(target, _ => new TargetView(target, Interlocked.Increment(ref this.targetOrdinal)));
+                this.targets.GetOrAdd(target, _ => new TargetView(target, Interlocked.Increment(ref this.targetCount)));
 
-            private static State Coalesce(State x, State y) =>
-                (State)Max((int)x, (int)y);
-
-            public class TargetView
+            private class SummaryRow
             {
-                private readonly ConcurrentDictionary<Guid, InputView> inputs = new ConcurrentDictionary<Guid, InputView>();
+                public string TargetOrInput { get; set; }
 
-                private int inputOrdinal;
+                public string State { get; set; }
 
-                public TargetView(string name, int ordinal)
-                {
-                    this.Name = name;
-                    this.Ordinal = ordinal;
-                }
+                public string Duration { get; set; }
 
-                public string Name { get; }
-
-                public int Ordinal { get; }
-
-                public State State { get; private set; }
-
-                public TimeSpan? Duration { get; private set; }
-
-                public IEnumerable<InputView> Inputs =>
-                    this.inputs.Values.OrderBy(input => input.Ordinal);
-
-                public TargetView Record(State state, TimeSpan? duration)
-                {
-                    this.State = Coalesce(this.State, state);
-                    this.Duration = this.Duration.Add(duration);
-                    return this;
-                }
-
-                internal (TargetView, InputView) Record(State state, TimeSpan? duration, object input, Guid inputId) =>
-                    (this, this.Record(state, duration).Record(inputId).Record(state, duration, input));
-
-                private InputView Record(Guid inputId) =>
-                    this.inputs.GetOrAdd(inputId, _ => new InputView(Interlocked.Increment(ref this.inputOrdinal)));
-
-                public class InputView
-                {
-                    public InputView(int ordinal) =>
-                        this.Ordinal = ordinal;
-
-                    public int Ordinal { get; }
-
-                    public State State { get; private set; }
-
-                    public TimeSpan? Duration { get; private set; }
-
-                    public object Input { get; private set; }
-
-                    public InputView Record(State state, TimeSpan? duration, object input)
-                    {
-                        this.State = Coalesce(this.State, state);
-                        this.Duration = this.Duration.Add(duration);
-                        this.Input = input;
-                        return this;
-                    }
-                }
+                public string Percentage { get; set; }
             }
         }
 
-        private enum State
+        private static TargetState Coalesce(TargetState x, TargetState y) =>
+            (TargetState)Max((int)x, (int)y);
+
+        private static InputState Coalesce(InputState x, InputState y) =>
+            (InputState)Max((int)x, (int)y);
+
+        private class TargetView
         {
-            Starting,
-            NoInputs,
-            Succeeded,
-            Failed,
+            private readonly ConcurrentDictionary<Guid, InputView> inputs = new ConcurrentDictionary<Guid, InputView>();
+
+            private int inputCount;
+
+            public TargetView(string name, int ordinal)
+            {
+                this.Name = name;
+                this.Ordinal = ordinal;
+            }
+
+            public string Name { get; }
+
+            public int Ordinal { get; }
+
+            public TargetState State { get; private set; }
+
+            public TimeSpan? Duration { get; private set; }
+
+            public IEnumerable<InputView> Inputs =>
+                this.inputs.Values.OrderBy(input => input.Ordinal);
+
+            public TargetView Record(TargetState state, TimeSpan? duration)
+            {
+                this.State = Coalesce(this.State, state);
+                this.Duration = this.Duration.Add(duration);
+                return this;
+            }
+
+            public (TargetView, InputView) Record(InputState state, TimeSpan? duration, object input, Guid inputId) =>
+                (this, this.Record((TargetState)state, duration).Record(inputId).Record(state, duration, input));
+
+            private InputView Record(Guid inputId) =>
+                this.inputs.GetOrAdd(inputId, _ => new InputView(Interlocked.Increment(ref this.inputCount)));
+        }
+
+        private class InputView
+        {
+            public InputView(int ordinal) =>
+                this.Ordinal = ordinal;
+
+            public int Ordinal { get; }
+
+            public InputState State { get; private set; }
+
+            public TimeSpan? Duration { get; private set; }
+
+            public object Input { get; private set; }
+
+            public InputView Record(InputState state, TimeSpan? duration, object input)
+            {
+                this.State = Coalesce(this.State, state);
+                this.Duration = this.Duration.Add(duration);
+                this.Input = input;
+                return this;
+            }
+        }
+
+        private enum TargetState
+        {
+            Starting = 0,
+            NoInputs = 1,
+            Succeeded = 2,
+            Failed = 3,
+        }
+
+        private enum InputState
+        {
+            Starting = 0,
+            Succeeded = 2,
+            Failed = 3,
         }
     }
 }
--- Bullseye/Internal/Logger.cs
+++ Bullseye/Internal/Logger.cs
@@ -7,7 +7,6 @@ namespace Bullseye.Internal
     using System.IO;
     using System.Linq;
     using System.Threading.Tasks;
-    using static System.Math;
 
     public partial class Logger
     {
@@ -62,19 +61,19 @@ namespace Bullseye.Internal
 
         public async Task Failed(List<string> targets)
         {
-            await this.WriteView().Tax();
+            await this.writer.WriteAsync(view.ToString(GetPrefix(), p)).Tax();
             await this.writer.WriteLineAsync(Message(p.Failed, $"Failed!", targets)).Tax();
         }
 
         public async Task Succeeded(List<string> targets)
         {
-            await this.WriteView().Tax();
+            await this.writer.WriteAsync(view.ToString(GetPrefix(), p)).Tax();
             await this.writer.WriteLineAsync(Message(p.Succeeded, $"Succeeded.", targets)).Tax();
         }
 
         public Task Starting(string target)
         {
-            var targetView = view.Record(target, State.Starting, null);
+            var targetView = view.Record(target, TargetState.Starting, null);
             return this.writer.WriteLineAsync(Message(p.Default, "Starting...", targetView));
         }
 
@@ -83,25 +82,25 @@ namespace Bullseye.Internal
 
         public Task Failed(string target, Exception ex, TimeSpan? duration)
         {
-            var targetView = view.Record(target, State.Failed, duration);
+            var targetView = view.Record(target, TargetState.Failed, duration);
             return this.writer.WriteLineAsync(Message(p.Failed, $"Failed! {ex.Message}", targetView));
         }
 
         public Task Failed(string target)
         {
-            var targetView = view.Record(target, State.Failed, null);
+            var targetView = view.Record(target, TargetState.Failed, null);
             return this.writer.WriteLineAsync(Message(p.Failed, $"Failed!", targetView));
         }
 
         public Task Succeeded(string target, TimeSpan? duration = null)
         {
-            var targetView = view.Record(target, State.Succeeded, duration);
+            var targetView = view.Record(target, TargetState.Succeeded, duration);
             return this.writer.WriteLineAsync(Message(p.Succeeded, "Succeeded.", targetView));
         }
 
         public Task Starting<TInput>(string target, TInput input, Guid inputId)
         {
-            var (targetView, inputView) = view.Record(target, State.Starting, null, input, inputId);
+            var (targetView, inputView) = view.Record(target, InputState.Starting, null, input, inputId);
             return this.writer.WriteLineAsync(Message(p.Default, "Starting...", targetView, inputView));
         }
 
@@ -110,107 +109,20 @@ namespace Bullseye.Internal
 
         public Task Failed<TInput>(string target, TInput input, Exception ex, TimeSpan? duration, Guid inputId)
         {
-            var (targetView, inputView) = view.Record(target, State.Failed, duration, input, inputId);
+            var (targetView, inputView) = view.Record(target, InputState.Failed, duration, input, inputId);
             return this.writer.WriteLineAsync(Message(p.Failed, $"Failed! {ex.Message}", targetView, inputView));
         }
 
         public Task Succeeded<TInput>(string target, TInput input, TimeSpan? duration, Guid inputId)
         {
-            var (targetView, inputView) = view.Record(target, State.Succeeded, duration, input, inputId);
+            var (targetView, inputView) = view.Record(target, InputState.Succeeded, duration, input, inputId);
             return this.writer.WriteLineAsync(Message(p.Succeeded, "Succeeded.", targetView, inputView));
         }
 
         public Task NoInputs(string target)
         {
-            var targetView = view.Record(target, State.NoInputs, null);
+            var targetView = view.Record(target, TargetState.NoInputs, null);
             return this.writer.WriteLineAsync(Message(p.Warning, "No inputs!", targetView));
-        }
-
-        private async Task WriteView()
-        {
-            // whitespace (e.g. can change to '·' for debugging)
-            var ws = ' ';
-
-            var rows = new List<SummaryRow> { new SummaryRow { TargetOrInput = $"{p.Default}Target{p.Reset}", State = $"{p.Default}Outcome{p.Reset}", Duration = $"{p.Default}Duration{p.Reset}", Percentage = "" } };
-
-            foreach (var targetView in view.Targets)
-            {
-                var target = $"{p.Target}{targetView.Name}{p.Reset}";
-
-                var state = targetView.State == State.Failed
-                    ? $"{p.Failed}Failed!{p.Reset}"
-                    : targetView.State == State.NoInputs
-                        ? $"{p.Warning}No inputs!{p.Reset}"
-                        : $"{p.Succeeded}Succeeded{p.Reset}";
-
-                var duration = targetView.Duration.HasValue
-                    ? $"{p.Timing}{targetView.Duration.Humanize()}{p.Reset}"
-                    : "";
-
-                var percentage = targetView.Duration.HasValue && view.Duration.HasValue && view.Duration.Value > TimeSpan.Zero
-                    ? $"{p.Timing}{100 * targetView.Duration.Value.TotalMilliseconds / view.Duration.Value.TotalMilliseconds:N1}%{p.Reset}"
-                    : "";
-
-                rows.Add(new SummaryRow { TargetOrInput = target, State = state, Duration = duration, Percentage = percentage });
-
-                var index = 0;
-
-                var inputs = targetView.Inputs.ToList();
-                foreach (var inputView in inputs)
-                {
-                    var input = $"{ws}{ws}{p.Input}{inputView.Input}{p.Reset}";
-
-                    var inputState = inputView.State == State.Failed ? $"{p.Failed}Failed!{p.Reset}" : $"{p.Succeeded}Succeeded{p.Reset}";
-
-                    var inputDuration = inputView.Duration.HasValue
-                        ? $"{(index < inputs.Count - 1 ? p.TreeFork : p.TreeCorner)}{p.Timing}{inputView.Duration.Humanize()}{p.Reset}"
-                        : "";
-
-                    var inputPercentage = inputView.Duration.HasValue && view.Duration.HasValue && view.Duration.Value > TimeSpan.Zero
-                        ? $"{(index < inputs.Count - 1 ? p.TreeFork : p.TreeCorner)}{p.Timing}{100 * inputView.Duration.Value.TotalMilliseconds / view.Duration.Value.TotalMilliseconds:N1}%{p.Reset}"
-                        : "";
-
-                    rows.Add(new SummaryRow { TargetOrInput = input, State = inputState, Duration = inputDuration, Percentage = inputPercentage });
-
-                    ++index;
-                }
-            }
-
-            // target or input column width
-            var tarW = rows.Max(row => Palette.StripColours(row.TargetOrInput).Length);
-
-            // outcome column width
-            var outW = rows.Max(row => Palette.StripColours(row.State).Length);
-
-            // duration column width
-            var durW = rows.Count > 1 ? rows.Skip(1).Max(row => Palette.StripColours(row.Duration).Length) : 0;
-
-            // percentage column width
-            var perW = rows.Max(row => Palette.StripColours(row.Percentage).Length);
-
-            // timing column width (duration and percentage)
-            var timW = Max(Palette.StripColours(rows[0].Duration).Length, durW + 2 + perW);
-
-            // expand percentage column width to ensure time and percentage are as wide as duration
-            perW = Max(timW - durW - 2, perW);
-
-            // summary start separator
-            await this.writer.WriteLineAsync($"{GetPrefix()}{p.Default}{"".Prp(tarW + 2 + outW + 2 + timW, p.Dash)}{p.Reset}").Tax();
-
-            // header
-            await this.writer.WriteLineAsync($"{GetPrefix()}{rows[0].TargetOrInput.Prp(tarW, ws)}{ws}{ws}{rows[0].State.Prp(outW, ws)}{ws}{ws}{rows[0].Duration.Prp(timW, ws)}").Tax();
-
-            // header separator
-            await this.writer.WriteLineAsync($"{GetPrefix()}{p.Default}{"".Prp(tarW, p.Dash)}{p.Reset}{ws}{ws}{p.Default}{"".Prp(outW, p.Dash)}{p.Reset}{ws}{ws}{p.Default}{"".Prp(timW, p.Dash)}{p.Reset}").Tax();
-
-            // targets
-            foreach (var row in rows.Skip(1))
-            {
-                await this.writer.WriteLineAsync($"{GetPrefix()}{row.TargetOrInput.Prp(tarW, ws)}{p.Reset}{ws}{ws}{row.State.Prp(outW, ws)}{p.Reset}{ws}{ws}{row.Duration.Prp(durW, ws)}{p.Reset}{ws}{ws}{row.Percentage.Prp(perW, ws)}{p.Reset}").Tax();
-            }
-
-            // summary end separator
-            await this.writer.WriteLineAsync($"{GetPrefix()}{p.Default}{"".Prp(tarW + 2 + outW + 2 + timW, p.Dash)}{p.Reset}").Tax();
         }
 
         private string Message(string color, string text) => $"{GetPrefix()}{color}{text}{p.Reset}";
@@ -223,13 +135,13 @@ namespace Bullseye.Internal
         private string Message(string color, string text, string target) =>
             $"{GetPrefix(target)}{color}{text}{p.Reset}";
 
-        private string Message(string color, string text, View.TargetView target) =>
+        private string Message(string color, string text, TargetView target) =>
             $"{GetPrefix(target.Name)}{color}{text}{p.Reset}{GetSuffix(true, target.Duration)}{p.Reset}";
 
         private string Message<TInput>(string color, string text, string target, TInput input) =>
             $"{GetPrefix(target, input)}{color}{text}{p.Reset}";
 
-        private string Message(string color, string text, View.TargetView target, View.TargetView.InputView input) =>
+        private string Message(string color, string text, TargetView target, InputView input) =>
             $"{GetPrefix(target.Name, input.Input)}{color}{text}{p.Reset}{GetSuffix(true, input.Duration)}{p.Reset}";
 
         private string GetPrefix() =>
@@ -249,16 +161,5 @@ namespace Bullseye.Internal
                 (!specific && this.parallel ? $" {p.Option}(parallel){p.Reset}" : "") +
                 (!specific && this.skipDependencies ? $" {p.Option}(skip dependencies){p.Reset}" : "") +
                 (duration.HasValue ? $" {p.Timing}({duration.Humanize()}){p.Reset}" : "");
-
-        private class SummaryRow
-        {
-            public string TargetOrInput { get; set; }
-
-            public string State { get; set; }
-
-            public string Duration { get; set; }
-
-            public string Percentage { get; set; }
-        }
     }
 }
